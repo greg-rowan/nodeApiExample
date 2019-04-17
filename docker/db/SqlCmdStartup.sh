614543329e96f1ad4ceb50ad6430e7bccf064904
Greg R <greg.rowan@macys.com>
2019-04-17 13:16:52 -0400

    Update SqlCmdStartup.sh

--- docker/db/SqlCmdStartup.sh
+++ docker/db/SqlCmdStartup.sh
@@ -1,2 +1,2 @@
 sleep 20s
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Alaska2017 -d master -i SqlCmdScript.sql
+/opt/mssql-tools/bin/sqlcmd -S db1 -U sa -P Alaska2017 -d master -i SqlCmdScript.sql
